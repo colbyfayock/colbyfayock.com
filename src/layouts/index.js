5e81fe5f0d5081515f885375544152ae3353e6f9
Colby Fayock <fayock@gmail.com>
2018-01-24 19:45:08 -0500

    adding gtm in post processing through netlify

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,14 +9,6 @@ import '../assets/scss/main.scss';
 import img_favicon_png from '../assets/images/favicon.png';
 import img_touchicon_png from '../assets/images/touchicon.png';
 
-const google_tag_manager = `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NJ75NM');
-`;
-
 const helmet_settings = {
     title: 'Colby Fayock - A UX Designer & Front-end Developer Blog',
     meta: [
