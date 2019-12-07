436d7902d959e6821871f65c4dcbdb4df350fabc
Colby Fayock <fayock@gmail.com>
2019-12-07 10:25:01 -0500

    adding tabs to hp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,62 +1,74 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import Layout from '../components/layout';
 import BlogList from '../components/blog-list';
 
 const Index = ({location, data}) => {
 
-    const helmet_settings = {
-        bodyAttributes: {
-            class: 'home',
-        }
-    };
+  const helmet_settings = {
+    bodyAttributes: {
+      class: 'home',
+    }
+  };
 
-    return (
-        <Layout location={location}>
-            <div className="container">
+  return (
+    <Layout location={location}>
+      <div className="container">
 
-                <Helmet {...helmet_settings} />
+        <Helmet {...helmet_settings} />
 
-                <BlogList posts={data.allMarkdownRemark.edges} />
+        <Tabs>
+          <TabList>
+            <Tab>Blog</Tab>
+            <Tab>Speaking</Tab>
+          </TabList>
+          <TabPanel>
+            <BlogList posts={data.allMarkdownRemark.edges} />
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
 
-            </div>
-        </Layout>
-    );
+      </div>
+    </Layout>
+  );
 
 }
 
 export default Index;
 
 export const pageQuery = graphql`
-    {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-        allMarkdownRemark(
-            sort: {
-                fields: [frontmatter___date],
-                order: DESC
-            }
-        )
-        {
-            edges {
-                node {
-                    excerpt
-                    frontmatter {
-                        template
-                        title
-                        date(formatString: "DD MMMM, YYYY")
-                        category
-                    }
-                    fields {
-                        slug
-                    }
-                }
-            }
-        }
+  {
+    site {
+      siteMetadata {
+        title
+      }
     }
+    allMarkdownRemark(
+      sort: {
+        fields: [frontmatter___date],
+        order: DESC
+      }
+    )
+    {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            template
+            title
+            date(formatString: "DD MMMM, YYYY")
+            category
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
 `;
