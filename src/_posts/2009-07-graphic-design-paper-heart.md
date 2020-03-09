a0d8184626483eea6caa663e9113be6da657718d
Colby Fayock <fayock@gmail.com>
2020-03-08 21:52:35 -0400

    figure component

--- src/_posts/2009-07-graphic-design-paper-heart.md
+++ src/_posts/2009-07-graphic-design-paper-heart.md
@@ -11,5 +11,3 @@ This project was created in response to a [poster design competition](http://new
 Link to my [official submission](http://colbz.deviantart.com/art/Paper-Heart-130156077?q=boost%3Apopular%20in%3Acontests%2F2009%2Fpaperheart&qo=71) for this contest.
 
 <figure><img src="/assets/paper-heart-poster-design.jpg" alt="" /><figcaption>Paper Heart Poster Submission</figcaption></figure>
-
-![Paper Heart](https://cdn.fay.io/images/2009/paper-heart-poster-design.jpg)
--- src/cms/components/figure.js
+++ src/cms/components/figure.js
@@ -1,4 +1,19 @@
-// Via https://gist.github.com/lnalex/2881c29649bc5d477954f79b3071e408
+// Based on https://gist.github.com/lnalex/2881c29649bc5d477954f79b3071e408
+
+/**
+ * createFigure
+ */
+
+function createFigure({ src, caption, alt = ''}) {
+  if ( !src || !caption ) return '';
+  return (
+    ''
+    + '<figure>'
+      + ( src ? '<img src="' + src + '" alt="' + alt + '" />' : '')
+      + ( caption ? '<figcaption>' + caption + '</figcaption>' : '')
+    + '</figure>'
+  )
+}
 
 export const figure = {
   id: 'figure',
@@ -7,65 +22,32 @@ export const figure = {
     {
       name: 'src',
       label: 'Image',
-      widget: 'image'
+      widget: 'image',
+      required: true
     },
     {
       name: 'caption',
       label: 'Caption',
-      widget: 'string'
+      widget: 'string',
+      required: true
     },
     {
-      name: 'link',
-      label: 'Link',
+      name: 'alt',
+      label: 'Alt',
       widget: 'string'
     }
   ],
-  pattern: /^{{<\s*figure (?:(src|caption|link)="([^"]*)")*\s*(?:(src|caption|link)="([^"]*)")*\s* (?:(src|caption|link)="([^"]*)")*\s*(?:(src|caption|link)="([^"]*)")*\s*>}}$/,
+  pattern: /^\s*<figure>\s*<img src="([^"]*)" alt="([^"]*)" \/>\s*(?:<figcaption>([^"]*)<\/figcaption>\s*)<\/figure>\s*$/,
   fromBlock: function(match) {
-
-    var src = '';
-    var caption = '';
-    var link = '';
-
-    for ( var i = 1; i < match.length; i += 2 ) {
-      if ( typeof match[i] === 'string' ) {
-        if ( match[i].includes('src') ) {
-          src = match[i + 1];
-        } else if ( match[i].includes('caption') ) {
-          caption = match[i + 1];
-        } else if ( match[i].includes('link') ) {
-          link = match[i + 1];
-        }
-      }
-    }
-
+    const src = match[1];
+    const alt = match[2];
+    const caption = match[3];
     return {
-      src: src,
-      caption: caption,
-      link: link
+      src,
+      caption,
+      alt
     };
   },
-  toBlock: function(obj) {
-    return '{{<figure src="' + obj.src + '" caption="' + obj.caption + '" link="'+ obj.link +'">}}';
-  },
-  toPreview: function(obj) {
-
-    var output = "";
-
-    if (obj.link.length > 0) {
-      output =
-        '<a href="'+ obj.link + '" target="_blank"><img src="' + obj.src + '"/></a>'
-      ;
-    } else {
-      output =
-        '<img src="' + obj.src + '"/>'
-      ;
-    }
-
-    if (obj.caption.length > 0) {
-      output += '<br> <i style="font-size: 0.7em">'+ obj.caption +'</i>';
-    }
-
-    return ( output );
-  }
+  toBlock: createFigure,
+  toPreview: createFigure
 }
