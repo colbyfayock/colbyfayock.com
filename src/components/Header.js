68ecd09b57cd93888f8c10609a07391f3ab9454b
colbyfayock <fayock@gmail.com>
2020-08-27 21:45:28 -0400

    fixing notice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 
 import Logo from './Logo';
 import SocialIcons from 'components/SocialIcons';
 
 const Header = ({location}) => {
-
-  const [notice, updateNotice] = useState();
-
-  useEffect(() => {
-    let updatedNotice;
-
-    if ( window.location.search.includes('emailSignup=success') ) {
-      updatedNotice = 'Thanks for signing up for my newsletter! 🤗';
-    } else if (window.location.search.includes('newsletterUnsubscribe=success')) {
-      updatedNotice = 'Sorry to see you go... 😢 Successfully unsubscribed from newsletter!';
-    }
-
-    if (updatedNotice) {
-      updateNotice(updatedNotice);
-      setTimeout(() => {
-        updateNotice(undefined);
-      }, 5000);
-    }
-  }, []);
-
   return (
-    <>
-      {notice && (
-        <div className="header-notice" onClick={() => updateNotice(false)}>
-          {notice}
-        </div>
-      )}
-      <nav className="header container" role="navigation">
-        <div className="content">
-          <div className="row">
+    <nav className="header container" role="navigation">
+      <div className="content">
+        <div className="row">
 
-            <div className="header-logo">
-              <Link to="/">
-                <Logo />
-              </Link>
-            </div>
-
-            <div className="header-social">
-              <SocialIcons />
-            </div>
-
+          <div className="header-logo">
+            <Link to="/">
+              <Logo />
+            </Link>
           </div>
+
+          <div className="header-social">
+            <SocialIcons />
+          </div>
+
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'gatsby';
 import { Helmet } from 'react-helmet';
 import { FaRss } from 'react-icons/fa';
@@ -11,6 +11,25 @@ import ArticleList from 'components/ArticleList';
 
 
 const Index = ({location, data}) => {
+
+  const [notice, updateNotice] = useState();
+
+  useEffect(() => {
+    let updatedNotice;
+
+    if ( window.location.search.includes('emailSignup=success') ) {
+      updatedNotice = 'Thanks for signing up for my newsletter! 🤗';
+    } else if (window.location.search.includes('newsletterUnsubscribe=success')) {
+      updatedNotice = 'Sorry to see you go... 😢 Successfully unsubscribed!';
+    }
+
+    if (updatedNotice) {
+      updateNotice(updatedNotice);
+      setTimeout(() => {
+        updateNotice(undefined);
+      }, 5000);
+    }
+  }, []);
 
   const { posts, toAll: toAllPosts } = usePosts();
   const { projects, toAll: toAllProjects } = useProjects();
@@ -39,6 +58,12 @@ const Index = ({location, data}) => {
   return (
     <Layout location={location}>
       <Helmet {...helmet_settings} />
+
+      {notice && (
+        <div className="header-notice" onClick={() => updateNotice(false)}>
+          {notice}
+        </div>
+      )}
 
       <Masthead />
 
