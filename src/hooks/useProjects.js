9676cc1e4fb4341640eaeb5c4102bd569bb72904
colbyfayock <fayock@gmail.com>
2020-09-16 23:15:32 -0400

    adding jamstack handbook to projects

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,5 +1,10 @@
 const FEATURED_PROJECTS = [
   {
+    path: 'https://jamstackhandbook.com/',
+    title: 'Jamstack Handbook',
+    category: 'Everything you need to know about creating fast, dynamic apps with the Jamstack including 3 step-by-step tutorials'
+  },
+  {
     path: 'https://50reactprojects.com/',
     title: '50 Projects for React & the Static Web',
     category: 'Free ebook with 50 project ideas'
