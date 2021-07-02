1aaceea07403fd391dec516f8278b8df734b9311
colbyfayock <fayock@gmail.com>
2021-07-01 20:16:06 -0400

    config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const sitemap = require('./plugins/sitemap');
 const socialImages = require('./plugins/socialImages');
 
 module.exports = withPlugins([[indexSearch], [feed], [sitemap], [socialImages]], {
+  target: 'serverless',
+
   // By default, Next.js removes the trailing slash. One reason this would be good
   // to include is by default, the `path` property of the router for the homepage
   // is `/` and by using that, would instantly create a redirect
