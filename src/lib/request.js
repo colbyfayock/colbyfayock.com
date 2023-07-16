bc0ded2528b6b008350e501989be17b3e733526a
Colby Fayock <fayock@gmail.com>
2023-07-16 17:13:37 -0400

    removing unneeded request cache

--- src/lib/request.js
+++ src/lib/request.js
@@ -1,33 +1,5 @@
 import { removeLastTrailingSlash } from 'lib/util';
 
-let cachedResult = {};
-
-// Temp hack to get around WP Engine / Netlify build issues with 504 timeouts
-
-export async function requestCachedData(request, cacheKey) {
-  // Check if the result is cached and the specified interval has not passed
-
-  if (!cachedResult[cacheKey]) {
-    cachedResult[cacheKey] = {
-      data: undefined,
-      time: 0,
-    };
-  }
-
-  if (cachedResult[cacheKey].data && Date.now() - cachedResult[cacheKey].time < 60000) {
-    return cachedResult[cacheKey].data;
-  }
-
-  // Perform the asynchronous operation (replace with your own async logic)
-  const result = await request;
-
-  // Cache the result and update the last fetch time
-  cachedResult[cacheKey].data = result;
-  cachedResult[cacheKey].time = Date.now();
-
-  return result;
-}
-
 export async function gql({ query, variables, method = 'POST' }) {
   const url = removeLastTrailingSlash(process.env.WORDPRESS_GRAPHQL_ENDPOINT);
   let data;
