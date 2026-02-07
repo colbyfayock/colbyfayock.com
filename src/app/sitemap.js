89f37504bc361d761fcecb6bb622a367526d7c25
Colby Fayock <fayock@gmail.com>
2026-02-07 10:28:23 -0500

    Restore sitemap.js for /sitemap.xml, remove redundant /api/sitemap route

---
+++ src/app/sitemap.js
@@ -0,0 +1,32 @@
+import { getSiteMetadata } from 'lib/site';
+import { getAllPages } from 'lib/pages';
+import { getAllPosts, postPathBySlug } from 'lib/posts';
+
+export default async function sitemap() {
+  const metadata = await getSiteMetadata();
+  const { pages: allPages } = await getAllPages();
+  const { posts: allPosts } = await getAllPosts();
+
+  const pages = allPages.map(({ modified, slug }) => {
+    return {
+      url: `${metadata.url}/${slug}`,
+      lastModified: new Date(modified).toISOString(),
+    };
+  });
+
+  const posts = allPosts.map(({ modified, slug }) => {
+    return {
+      url: `${metadata.url}${postPathBySlug(slug)}`,
+      lastModified: new Date(modified).toISOString(),
+    };
+  });
+
+  return [
+    {
+      url: metadata.url,
+      lastModified: new Date().toISOString(),
+    },
+    ...pages,
+    ...posts,
+  ];
+}
